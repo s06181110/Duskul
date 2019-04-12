2409e1c5cbc5d3f0354cefd49bebebecae0ba914
s06181110 <s06181110@gmail.com>
2019-04-12 17:01:56 +0900

    Initial Commit

---
+++ dusk/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dusk
+//
+//  Created by 榎本啓希 on 2019/04/12.
+//  Copyright © 2019 s06181110. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
